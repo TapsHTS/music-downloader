b909834a5c9474eeb41da970b2746891cd7ca1b7
Snowflake <46562212+Snowflake107@users.noreply.github.com>
2020-07-01 23:01:18 +0545

    initial release

---
+++ index.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const ytdl = require("ytdl-core");
+const ytsr = require("ytsr");
+const app = express();
+
+app.use(express.static("style"));
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", async (req, res) => {
+    return res.render("index", {
+        error: null
+    });
+});
+
+app.post("/", async (req, res) => {
+    let video = req.body.video;
+    let type = req.body.type || "play";
+    if (!video) return res.render("index", {
+        error: "No video provided!"
+    });
+    return await downloadVideo(video, type.toLowerCase(), res);
+});
+
+app.get("*", (req, res) => {
+    return res.redirect("/");
+});
+
+async function downloadVideo(video, type, res) {
+    if (!video) return;
+    try {
+        var searchResult = await ytsr(video);
+    } catch(e) {
+        return res.render("index", {
+            error: "Couldn't find the video!"
+        });
+    }
+    if (!searchResult) return res.render("index", {
+        error: "Couldn't find the video!"
+    });
+    let pickOne = searchResult.items.filter(i => i.type === "video");
+    if (!pickOne || pickOne.length < 1) return res.render("index", {
+        error: "Couldn't find the video!"
+    });
+    
+    if (type === "play") {
+        try {
+            var downloaded = await ytdl.getInfo(pickOne[0].link, {
+                filter: (f) => f.container === "mp4",
+                quality: "heighest"
+            });
+        } catch (e) {
+            return res.render("index", {
+                error: "Couldn't find the video!"
+            });
+        }
+        if (!downloaded) return res.render("index", {
+            error: "Couldn't find the video!"
+        });
+        return res.render("player", {
+            stream: downloaded.formats[0].url,
+            data: pickOne[0]
+        });
+    } else {
+        try {
+            var downloadedStream;
+            if (type !== "audio") downloadedStream = await ytdl(pickOne[0].link, {
+                quality: "highestvideo",
+                filter: (fmt) => fmt.container === "mp4"
+            });
+            else downloadedStream = await ytdl(pickOne[0].link, {
+                quality: "highestaudio"
+            });
+        } catch (e) {
+            return res.render("index", {
+                error: "Couldn't find the video!"
+            });
+        }
+        if (!downloadedStream) return res.render("index", {
+            error: "Couldn't find the video!"
+        });
+        res.header("Content-Disposition", `attachment; filename="${pickOne[0].title}.${type == "audio" ? "mp3" : "mp4"}"`);
+        return downloadedStream.pipe(res);
+    }
+}
+
+app.listen(3000, () => console.log("Server started!"));
