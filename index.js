aec69be7ccde8057fd6c41e7e7c2fa9a14172474
TapsHTS <61658427+TapsHTS@users.noreply.github.com>
2021-12-25 21:54:39 +0100

    🎄XMas Update | 2.1.4

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ const ytdl = require("ytdl-core");
 const app = express();
 const youtube = require('youtube-sr');
 const cors = require('cors');
+const fs = require('fs');
+
+var today = new Date();
+var dd = today.getDate();
+
+var mm = today.getMonth() + 1;
+var yyyy = today.getFullYear();
+if (dd < 10) {
+    dd = '0' + dd;
+}
+
+if (mm < 10) {
+    mm = '0' + mm;
+}
+
+const date = `${dd}/${mm}/${yyyy}`
 
 app.use(cors());
 app.use(express.static("style"));
@@ -10,29 +26,88 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", async (req, res) => {
+app.get("/", async(req, res) => {
     return res.render("index", {
         error: null
     });
 });
 
-async function downloadVideo(video, type, res) {
-        if (!video) return;
-        youtube.search(video, { limit: 1 }).then(async video => {
-            if (!video[0]) return res.render("index", { error: "❌ Impossible de trouver la musique !" })
-
-            ytdl(video[0].id, { quality: 'highestaudio' }).pipe(res.attachment(video[0].title + '.mp3'));
-        }).catch(err => res.render("index", { error: "❌ Impossible de trouver la musique !"}));
-    
+function validURL(str) {
+    var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+        '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+    return !!pattern.test(str);
 }
 
-app.post("/", async (req, res) => {
+function YouTubeGetID(url) {
+    var ID = '';
+    url = url.replace(/(>|<)/gi, '').split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
+    if (url[2] !== undefined) {
+        ID = url[2].split(/[^0-9a-z_\-]/i);
+        ID = ID[0];
+    } else {
+        ID = url;
+    }
+    return ID;
+}
+
+async function downloadVideo(video, type, res, req) {
+    if (!video) return;
+    if (validURL(video)) {
+        try {
+            const data = await ytdl.getBasicInfo(YouTubeGetID(video))
+            const min = Math.floor(data.videoDetails.lengthSeconds / 60);
+            const seconds = data.videoDetails.lengthSeconds - min * 60;
+
+            ytdl("https://youtube.com/watch?v=" + YouTubeGetID(video), { quality: 'highestaudio' })
+                .pipe(fs.createWriteStream(data.videoDetails.title + '.mp3'))
+            setTimeout(() => {
+                res.download(data.videoDetails.title + '.mp3', data.videoDetails.title + '.mp3', function(err) {
+                    fs.unlink(data.videoDetails.title + '.mp3', (err) => {
+                        if (err) throw err;
+                        console.log(err)
+                    });
+                });
+            }, 800);
+            //la sa met le fichier dans un dossier music_cache
+            // sa download le fichier
+
+        } catch (err) {
+            res.render("index", { error: "❌ Impossible de trouver la musique !" })
+            console.log(err)
+        }
+
+    } else {
+        youtube.search(video, { limit: 1 })
+            .then(async video => {
+                if (!video[0]) return res.render("index", { error: "❌ Impossible de trouver la musique !" })
+				const data = await ytdl.getBasicInfo(YouTubeGetID(video[0].id))
+                
+                ytdl("https://youtube.com/watch?v=" + video[0].id, { quality: 'highestaudio' })
+                    .pipe(fs.createWriteStream(data.videoDetails.title + '.mp3'))
+                    setTimeout(() => {
+                    res.download("./" + data.videoDetails.title + '.mp3', data.videoDetails.title + '.mp3', function(err) {
+                        fs.unlink(data.videoDetails.title + '.mp3', (err) => {
+                            if (err) throw err;
+                        });
+                    });
+                }, 800);
+
+
+            })
+            .catch(err => res.render("index", { error: "❌ Impossible de trouver la musique !" }));
+    }
+}
+app.post("/", async(req, res) => {
     let video = req.body.video;
     let type = req.body.type || "play";
     if (!video) return res.render("index", {
         error: "Aucune vidéo fournie!"
     });
-    return await downloadVideo(video, type.toLowerCase(), res);
+    return await downloadVideo(video, type.toLowerCase(), res, req);
 });
 
 app.get("*", (req, res) => {
@@ -41,4 +116,6 @@ app.get("*", (req, res) => {
 
 
 
-app.listen(8080, () => console.log("Site démarer !"));
+app.listen(30000, () => console.log("Site démarer !"));
+
+//download mp3 file with ytdl core and store it in the server and send it to the client with express
